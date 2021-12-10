e535c6d05f041c5c57e400934ad99ea62e256b9f
aamarilla <fangelith@gmail.com>
2021-12-10 10:00:05 -0300

    cambios para test de subida

--- src/App/components/health/index.ts
+++ src/App/components/health/index.ts
@@ -6,4 +6,8 @@ router.get('/', (req: Request, res: Response) => {
     res.send('<p>Si funciona</p>');
 });
 
+router.get('/2', (req: Request, res: Response) => {
+    res.send('<p>Si funciona 2</p>');
+});
+
 export default router;
