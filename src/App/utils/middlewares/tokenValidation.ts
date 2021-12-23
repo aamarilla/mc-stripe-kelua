f0d0b980831ed81c9ba461a3bc2870c9ef62a776
Miguel Ramirez <miguelramidev@gmail.com>
2021-12-23 11:22:15 -0300

    Se devuelve una respuesta cuando da error el middleware de validación de token

--- src/App/utils/middlewares/tokenValidation.ts
+++ src/App/utils/middlewares/tokenValidation.ts
@@ -1,12 +1,16 @@
 import {NextFunction, Request, Response} from 'express';
-import {decrypt} from '..';
+import {decrypt, response} from '..';
 import config from '../../config';
 
 export const tokenValidation = (req: Request, res: Response, next: NextFunction): void => {
-    if (req.method !== 'GET' && req.method !== 'DELETE') {
-        const bodyDecrypt = decrypt(String(req.body), config.appSecretKey);
+    try {
+        if (req.method !== 'GET' && req.method !== 'DELETE') {
+            const bodyDecrypt = decrypt(String(req.body), config.appSecretKey);
 
-        req.body = JSON.parse(bodyDecrypt);
+            req.body = JSON.parse(bodyDecrypt);
+        }
+        next();
+    } catch (error) {
+        response.error({res, data: error, status: 401, errorCode: -1});
     }
-    next();
 };
